cdf78b356d67d577c2ac5e634210cd75cde29bb2
mnasser480 <Mm0119734263..>
2024-03-17 11:53:42 +0200

    add snipped CRUD dealing with data file instead of mongo

--- src/snippets/snippet.service.ts
+++ src/snippets/snippet.service.ts
@@ -3,37 +3,50 @@ import {InjectModel} from "@nestjs/mongoose";
 import {Model} from "mongoose";
 import {Snippet} from "./snippet.schema";
 import {CreateSnippetDto} from "./dtos/create-snippet-dto";
+import * as path from "path";
+import * as fs from "fs";
 
 @Injectable()
 export class SnippetsService {
-
+    private readonly dataFilePath = path.resolve(__dirname, '../../src/data/snippets.json');
     constructor(@InjectModel("Snippet") private snippetModel: Model<Snippet>) {}
-    async create(createSnippetDto: CreateSnippetDto): Promise<Snippet> {
-        const createdSnippet = new this.snippetModel(createSnippetDto);
-        return createdSnippet.save();
+    async create(createSnippetDto: CreateSnippetDto): Promise<{ id: string, title: string, code: string}> {
+        const snapped = JSON.parse(fs.readFileSync(this.dataFilePath, 'utf-8'));
+        const id = Math.floor(Math.random() * 999);
+        snapped.push({id, ...createSnippetDto});
+        fs.writeFileSync(this.dataFilePath, JSON.stringify(snapped, null, 2), 'utf-8');
+        return {
+            id: JSON.stringify(id),
+            ...createSnippetDto
+        };
     }
 
     async findAll(): Promise<Snippet[]> {
-        return this.snippetModel.find();
-    }
-
-    async find(id: string){
-        return this.snippetModel.findById(id);
+        const snippets = fs.readFileSync(this.dataFilePath, 'utf-8');
+        return JSON.parse(snippets);
     }
 
     async update(id: string, attrs: Partial<Snippet>){
-        const snippet = await this.snippetModel.findById(id);
-        if(!snippet){
-            throw new Error('snippet not found');
+        const snippets = fs.readFileSync(this.dataFilePath, 'utf-8');
+        const jsonSnippets = JSON.parse(snippets);
+        const index = jsonSnippets.findIndex( (snippet:any) => snippet.id == id);
+        if(index === -1 ){
+            throw new NotFoundException('snippet not found');
         }
-        Object.assign(snippet, attrs);
-        return snippet.save();
+        jsonSnippets[index] = {...jsonSnippets[index], ...attrs};
+        fs.writeFileSync(this.dataFilePath, JSON.stringify(jsonSnippets, null, 2), 'utf-8');
+        return jsonSnippets;
     }
 
     async remove(id: string){
-        const snippet = await this.snippetModel.findOne({_id: id});
-        if(!snippet)
-            throw new NotFoundException('Snippet not found');
-        return this.snippetModel.findByIdAndDelete(id);
+        const snippets = fs.readFileSync(this.dataFilePath, 'utf-8');
+        const jsonSnippets = JSON.parse(snippets);
+        const index = jsonSnippets.findIndex( (snippet:any) => snippet.id == id);
+        if(index === -1 ){
+            throw new NotFoundException('snippet not found');
+        }
+        jsonSnippets.splice(index, 1);
+        fs.writeFileSync(this.dataFilePath, JSON.stringify(jsonSnippets, null, 2), 'utf-8');
+        return jsonSnippets;
     }
 }
